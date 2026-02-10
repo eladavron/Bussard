92aae071b56b75887d1c4063fb06f79d9d8e1148
Elad Avron <eladavron@gmail.com>
2026-02-10 11:25:45 +0200

    Added repo to docker tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
 
   return (
     <>
-      <div id="top" />Hi, call
+      <div id="top" />
       <Header refreshMovies={refreshMovies} />
       <TopBar
         movies={allMovies}
