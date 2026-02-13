63b7ccd6ba04eeed10a0e6d48698dd5afa57b469
Elad Avron <eladavron@gmail.com>
2026-02-13 17:59:14 +0200

    Added Barcode Scanning

--- src/app/actions/omdb.ts
+++ src/app/actions/omdb.ts
@@ -7,8 +7,12 @@ export async function searchOMDB(query: string): Promise<OMDBResult | OMDBMovieE
         throw new Error('Query parameter is required');
     }
 
+    if (IMDBPattern.test(query)) {
+        return await searchOMDBByParameter({ imdbID: query });
+    }
+
     const params = new URLSearchParams();
-    params.append(query.match(IMDBPattern) ? 'i' : 's', query);
+    params.append('s', query);
     params.append('apikey', process.env.OMDB_API_KEY || '');
 
     const response = await fetch(`http://www.omdbapi.com/?${params.toString()}`);
@@ -16,4 +20,64 @@ export async function searchOMDB(query: string): Promise<OMDBResult | OMDBMovieE
         throw new Error(`OMDB API error: ${response.statusText}`);
     }
     return await response.json();
+}
+
+
+interface searchParameters {
+    title?: string;
+    imdbID?: string;
+    year?: number;
+}
+
+export async function searchOMDBByParameter(params: searchParameters): Promise<OMDBResult | OMDBMovieExtended> {
+    const urlParams = new URLSearchParams();
+    if (params.title) {
+        urlParams.append('t', params.title);
+    }
+    if (params.imdbID) {
+        urlParams.append('i', params.imdbID);
+    }
+    if (params.year) {
+        urlParams.append('y', params.year.toString());
+    }
+    urlParams.append('apikey', process.env.OMDB_API_KEY || '');
+
+    const response = await fetch(`http://www.omdbapi.com/?${urlParams.toString()}`);
+    if (!response.ok) {
+        throw new Error(`OMDB API error: ${response.statusText}`);
+    }
+    return await response.json();
+}
+
+
+export async function searchByBarcode(barcode: string): Promise<OMDBMovieExtended> {
+    if (!barcode) {
+        throw new Error('Barcode parameter is required');
+    }
+    const params = new URLSearchParams();
+    params.append("key", process.env.BARCODE_LOOKUP_API_KEY || '');
+    params.append("barcode", barcode);
+    const response = await fetch(`https://api.barcodelookup.com/v3/products?${params.toString()}`);
+    if (!response.ok) {
+        throw new Error(`Barcode Lookup API error: ${response.statusText}`);
+    }
+    const data = await response.json();
+    if (!data.products || data.products.length === 0) {
+        throw new Error('No product found for the given barcode');
+    }
+    const product = data.products[0];
+    if (!product.title) {
+        throw new Error('Product does not have a title');
+    }
+
+    if (product.release_date) {
+        const releaseYear = new Date(product.release_date).getFullYear();
+        const omdbData = await searchOMDBByParameter({ title: product.title, year: releaseYear });
+        if ('Search' in omdbData) {
+            throw new Error('No exact match found for the given product');
+        }
+        return omdbData as OMDBMovieExtended;
+    }
+
+    return await searchOMDBByParameter({ title: product.title }) as OMDBMovieExtended;
 }
--- src/components/modals/SearchModal.tsx
+++ src/components/modals/SearchModal.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import BaseModal from './BaseModal';
 import { OMDBMovieExtended, OMDBResult } from '../../types/omdb';
 import MovieResultRow from './MovieResultRow';
-import { searchOMDB } from '../../app/actions/omdb';
-import { Tooltip } from '@heroui/react';
+import { searchByBarcode, searchOMDB, searchOMDBByParameter } from '../../app/actions/omdb';
+import { Tab, Tabs, Tooltip } from '@heroui/react';
+import { IoBarcodeOutline, IoSearch } from "react-icons/io5";
+import { BrowserMultiFormatReader } from '@zxing/browser';
+import { CgSpinner } from 'react-icons/cg';
 
 interface SearchModalProps {
     isOpen: boolean;
@@ -25,7 +28,12 @@ export default function SearchModal({ isOpen, setIsOpen, refreshMovies }: Search
     const [errorMessage, setErrorMessage] = useState<[string, string]>(['', '']);
     const [loadedMovieIDs, setLoadedMovieIDs] = useState<Set<string>>(new Set());
     const [addMultiple, setAddMultiple] = useState<boolean>(false);
+    const [selectedTab, setSelectedTab] = useState<string>('search');
     const movieIDsBeingFetched = useRef<Set<string>>(new Set());
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const streamRef = useRef<MediaStream>(null);
+    const [scanResults, setScanResults] = useState<OMDBMovieExtended | null>(null);
+    const [scanResultLoading, setScanResultLoading] = useState<boolean>(false);
 
     // Debounce timer reference
     useEffect(() => {
@@ -62,13 +70,53 @@ export default function SearchModal({ isOpen, setIsOpen, refreshMovies }: Search
         }
     }, [isOpen]);
 
+    const stopCamera = () => {
+
+        if (streamRef.current) {
+            streamRef.current.getTracks().forEach(track => track.stop());
+            streamRef.current = null;
+        }
+    }
+
+    useEffect(() => {
+        const codeReader = new BrowserMultiFormatReader();
+        if (selectedTab !== 'barcode' || scanResults) {
+            stopCamera();
+            return;
+        }
+        if (videoRef.current) {
+            codeReader.decodeFromVideoDevice(
+                undefined, // use default camera
+                videoRef.current,
+                async (result, error, controls) => {
+                    if (videoRef.current && videoRef.current.srcObject) {
+                        streamRef.current = videoRef.current.srcObject as MediaStream;
+                    }
+                    if (result) {
+                        controls.stop(); // stop scanning if you want
+                        const barcode = result.getText();
+                        setScanResultLoading(true);
+                        const results = await searchByBarcode(barcode);
+                        setScanResults(results);
+                        setScanResultLoading(false);
+                    }
+                    // handle error if needed
+                },
+            );
+        }
+
+        return () => {
+            stopCamera();
+        }
+    }, [selectedTab, scanResults]);
+
     async function getMoreData(imdbID: string): Promise<void> {
         if (movieIDsBeingFetched.current.has(imdbID)) {
             return;
         }
         movieIDsBeingFetched.current.add(imdbID);
         try {
-            const data = await searchOMDB(imdbID) as OMDBMovieExtended;
+            const data = await searchOMDBByParameter({ imdbID }) as OMDBMovieExtended;
             extendedDataCache.set(imdbID, data);
             setOmdbResults((prev) => prev.map((movie) => (
                 movie.imdbID === imdbID ? { ...movie, ...data } : movie
@@ -105,58 +153,85 @@ export default function SearchModal({ isOpen, setIsOpen, refreshMovies }: Search
 
     return (
         <BaseModal
-            title={
-                <span className="flex flex-col gap-2">
-                    <span>Search by IMDB ID or Title...</span>
-                </span>
+            title="Search by IMDB ID or Title"
+            isOpen={isOpen} onClose={() => {
+                stopCamera();
+                setIsOpen(false);
             }
-            isOpen={isOpen} onClose={() => setIsOpen(false)}
-                className="w-full max-w-full sm:max-w-2xl"
-            body={<>
-                <div className="flex justify-center">
-                    <div className='w-full mb-0 flex flex-col gap-1 items-end'>
-                        <Tooltip color='foreground' content='Add multiple movies from search results without closing the modal' placement='top' closeDelay={0}>
-                            <label className="text-sm text-secondary cursor-pointer flex items-center gap-2 float-end">
-                                Add Multiple
-                                <input id="addMultiple" type="checkbox" checked={addMultiple} onChange={(e) => setAddMultiple(e.target.checked)} />
-                            </label>
-                        </Tooltip>
-                        <input
-                            className='input-default w-full'
-                            name='search'
-                            placeholder='tt1234567 or "Interstellar"...'
-                            autoComplete="off"
-                            value={searchQuery}
-                            onChange={(e) => {
-                                setSearchQuery(e.target.value);
-                            }}
-                        />
-                    </div>
-                </div>
-                {errorMessage && <div className="text-danger">{errorMessage[0]}<br /><pre>{errorMessage[1]}</pre></div>}
-                {loading && (
-                    <ul>
-                        <MovieResultRow key="skeleton1" isLoading={true} />
-                        <MovieResultRow key="skeleton2" isLoading={true} />
-                        <MovieResultRow key="skeleton3" isLoading={true} />
-                    </ul>
-                )}
-                {!loading && !errorMessage[0] && searchQuery && <div>
-                    {omdbResults && omdbResults.length > 0 ? (
-                        <ul>
-                            {omdbResults.map((movie) => (
-                                <MovieResultRow key={movie.imdbID} movie={movie} extendedDataLoaded={loadedMovieIDs.has(movie.imdbID)} onAdd={async () => {
+            }
+            className="w-full max-w-full sm:max-w-2xl"
+            body={
+
+                <Tabs selectedKey={selectedTab} onSelectionChange={(key: React.Key) => setSelectedTab(key.toString())} className="w-full">
+                    <Tab key="search" title={<div className='flex items-center gap-2'><IoSearch /> Search</div>}>
+                        <div className="flex justify-center">
+                            <div className='w-full mb-0 flex flex-col gap-1 items-end'>
+                                <Tooltip color='foreground' content='Add multiple movies from search results without closing the modal' placement='top' closeDelay={0}>
+                                    <label className="text-sm text-secondary cursor-pointer flex items-center gap-2 float-end">
+                                        Add Multiple
+                                        <input id="addMultiple" type="checkbox" checked={addMultiple} onChange={(e) => setAddMultiple(e.target.checked)} />
+                                    </label>
+                                </Tooltip>
+                                <input
+                                    className='input-default w-full'
+                                    name='search'
+                                    placeholder='tt1234567 or "Interstellar"...'
+                                    autoComplete="off"
+                                    value={searchQuery}
+                                    onChange={(e) => {
+                                        setSearchQuery(e.target.value);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                        {errorMessage && <div className="text-danger">{errorMessage[0]}<br /><pre>{errorMessage[1]}</pre></div>}
+                        {loading && (
+                            <ul>
+                                <MovieResultRow key="skeleton1" isLoading={true} />
+                                <MovieResultRow key="skeleton2" isLoading={true} />
+                                <MovieResultRow key="skeleton3" isLoading={true} />
+                            </ul>
+                        )}
+                        {!loading && !errorMessage[0] && searchQuery && <div>
+                            {omdbResults && omdbResults.length > 0 ? (
+                                <ul>
+                                    {omdbResults.map((movie) => (
+                                        <MovieResultRow key={movie.imdbID} movie={movie} extendedDataLoaded={loadedMovieIDs.has(movie.imdbID)} onAdd={async () => {
+                                            await refreshMovies();
+                                            if (!addMultiple) {
+                                                setIsOpen(false);
+                                            }
+                                        }} />
+                                    ))}
+                                </ul>
+                            ) : (!loading && <p className="text-primary">No results found.</p>)
+                            }
+                        </div>
+                        }
+                    </Tab>
+                    <Tab key="barcode" title={<div className='flex items-center gap-2'><IoBarcodeOutline /> Barcode</div>}>
+                        {!scanResults && !scanResultLoading && <video ref={videoRef} style={{ width: '100%' }} />}
+                        {!scanResults && scanResultLoading && (<div className="flex flex-col items-center gap-4">
+                            <div className="w-full">
+                                <MovieResultRow isLoading={true} />
+                            </div>
+                        </div>
+                        )}
+                        {scanResults &&
+                            <div>
+                                <h2 className="text-lg font-bold mb-4">Scan Results:</h2>
+                                <MovieResultRow isLoading={scanResultLoading} movie={scanResults} extendedDataLoaded={true} onAdd={async () => {
                                     await refreshMovies();
-                                    if (!addMultiple) {
-                                        setIsOpen(false);
-                                    }
+                                    setScanResults(null);
                                 }} />
-                            ))}
-                        </ul>
-                    ) : (!loading && <p className="text-primary">No results found.</p>)
-                    }
-                </div>}
-            </>
+                                <button className="button-secondary float-end flex gap-2 items-stretch" onClick={() => setScanResults(null)}>
+                                    <IoBarcodeOutline className="size-auto" />
+                                    Scan Again
+                                </button>
+                            </div>
+                        }
+                    </Tab>
+                </Tabs>
             }
         />
     );
